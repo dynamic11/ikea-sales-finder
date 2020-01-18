b77d1c4df2df7544d40e96ba2a91e2053d8fc185
Ali Nouri <dynamic11@gmail.com>
2020-01-18 14:08:16 -0500

    added ability to search all possible capitalizations of a word

--- main3.py
+++ main3.py
@@ -1,25 +1,42 @@
 from selenium import webdriver
 import re
+import string
 from selenium.webdriver.firefox.options import Options
 
+# configure and start selenium webdriver
 options = Options()
 options.headless = True
 #driver = webdriver.Firefox(options=options, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
 driver = webdriver.Firefox(options=options)
 
-print('Going to webpage')
-driver.get("https://www.ikea.com/ca/en/")
+url="https://www.ikea.com/ca/en/"
+searchTerm = "Storage Event"
+
+# check number of word to determine all possible capitalization of the search term
+if(len(searchTerm.split()) > 1 ):
+    possibleSearchTerms = [searchTerm.lower(), searchTerm.upper(), string.capwords(searchTerm), searchTerm.capitalize()]
+else:
+    possibleSearchTerms = [searchTerm.lower(), searchTerm.upper(), searchTerm.capitalize()]
+
+print("Possible Search terms:", possibleSearchTerms)
+
+# got to desired webpage
+print('Going to webpage:', url)
+driver.get(url)
 try:
     print('Start Search')
-    zz = driver.page_source
-    numbResults = len(re.findall(r"storage", zz, re.IGNORECASE))
-    print(numbResults)
-    ll = driver.find_elements_by_xpath("//*[contains(text(), 'Storage Event')]")
-    print(len(ll))
-    for ii in ll:
-        # print ii.tag_name
-        print(ii.text)  # id name as string
-    # print(len(re.findall(r"storage", zz, re.IGNORECASE)))
+    numbResults = 0
+    for possibleSearchTerm in possibleSearchTerms:
+
+        # search for term
+        print("Searching for Term:", possibleSearchTerm)
+        xpath = "//*[contains(text(), '" + possibleSearchTerm + "')]"
+        results = driver.find_elements_by_xpath(xpath)
+
+        # increment search results
+        numbResults = numbResults + len(results)
+        print(numbResults)
+    print("Total Results Found:", numbResults)
     print('Search Done')
 finally:
     driver.quit()
